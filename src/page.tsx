4217e813c127eb7fb9ce8767522bf9e68aed47f2
sidecus <4399408+sidecus@users.noreply.github.com>
2020-03-19 21:11:11 -0700

    add more comments

--- src/page.tsx
+++ src/page.tsx
@@ -6,19 +6,19 @@ import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 const INVALID_PAGEID = 0;
 
-// Routing parameters we are expecting.
+// Routing parameters we are expecting, accessed via this.props.match.params
 interface IRouteParams {
     id?: string;
 }
 
-// IOwnProps type to receive routing props as well as passed in component props 
+// IOwnProps type to receive both routing params and props from parent component
 type IOwnProps = RouteComponentProps<IRouteParams> & {
     pageName: string;
 }
 
 const getPageIdFromRoute = (props: IOwnProps) => !props.match.params.id ? INVALID_PAGEID : parseInt(props.match.params.id!);
 
-// use ownProps to filter state
+// mapStateToProps - note we are using ownProps to filter the state but it's not necessary, if not needed just don't take the second parameter
 const mapState = (store: RootState, ownProps: IOwnProps) => {
     const pageId = getPageIdFromRoute(ownProps);
     const validPageId = pageId >= 1 && pageId < store.ClickCounts.length ? pageId : INVALID_PAGEID;
@@ -28,13 +28,13 @@ const mapState = (store: RootState, ownProps: IOwnProps) => {
     };
 };
 
-// using the object short hand for map dispatch to props. No need to call bindActionCreators manually
+// mapDispatchToProps - we are using the object short hand for map dispatch to props. No need to call bindActionCreators manually
 // https://react-redux.js.org/using-react-redux/connect-mapdispatch#defining-mapdispatchtoprops-as-an-object
 const mapDispatch = {
     increaseClickCount
 };
 
-// Interring the connected prop type automatically 
+// Interring the connected prop type automatically. We need it for the class component definition
 // https://react-redux.js.org/using-react-redux/static-typing#inferring-the-connected-props-automatically
 const connector = connect(mapState, mapDispatch);
 type IRoutePageProps = IOwnProps & ConnectedProps<typeof connector>;
